82b6d0abf9be52d0d5b7c80f2c406f6c915f7e01
Kevin Levasseur <kevin_levasseur@telus.net>
2023-10-26 15:15:29 -0700

    File type and name change

---
+++ Practice.js
@@ -0,0 +1,11 @@
+//my tinkering file
+let Num1 = 2
+let Num2 = 5
+
+if (Num1 > Num2){
+    console.log("Num1 is greater");
+} else if (Num1 == Num2){
+    console.log("Num1 is eqaul to Num2");
+} else {
+    console.log("Num2 is greater");
+}
